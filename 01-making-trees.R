8e55564a1b67380ef987b58b46337f6c99ef8da7
Natalie Cooper <ncooper@tcd.ie>
2017-10-30 18:06:16 +0000

    Add analysis file to create new trees and extract node counts

---
+++ 01-making-trees.R
@@ -0,0 +1,90 @@
+# Analysis - step 1
+# Create trees and extract node counts
+
+# Load libraries and source functions
+library(ape)
+library(geiger)
+source("functions/branch_in_period.R")
+source("functions/break_branches_select_age.R")
+source("functions/extract_clade_trees.R")
+source("functions/make_trees.R")
+source("functions/get_node_count.R")
+
+# Read in phylogeny and taxonomy data
+tree <- read.tree("data/trees/lloyd2008_midpoint.tre")
+taxonomy <- read.csv("data/taxonomy.data.csv")
+
+#--------------------------------------------------------------------
+# For original tree
+#--------------------------------------------------------------------
+# Create clade tree and write to file
+tree.list <- extract_clade_trees(tree, taxonomy, 
+                                 species.col.name = "taxon", 
+                                 clade.col.name = "clade")
+# Write clade trees 
+write.tree(phy = tree.list, 
+           file = paste0("data/trees/lloyd2008_midpoint_clades.tre"), 
+           tree.names = TRUE)
+
+# Get node count for full tree
+nodecount <- get_node_count(tree)
+write.csv(nodecount, "data/nodecounts/nodecount_lloyd2008.csv")
+
+# Loop through each clade  
+for(j in 1:length(tree.list)){
+  # Split multi phylo into one tree
+  tree <- tree.list[[j]]
+  # Get node counts
+  create_node_counts(tree, path = "data/nodecounts/", 
+                     simulation.name = paste0("nodecount_lloyd2008_", 
+                                              names(tree.list[j])))
+}
+
+#--------------------------------------------------------------------
+# For trees with added taxa
+#--------------------------------------------------------------------
+# Create list of numbers.to.add
+# These are 1%, 5%, 10%, 25% and 50% additional taxa
+# out of 420 in the Lloyd tree
+numbers.to.add <- c(4, 21, 42, 105, 210)
+
+# Create all the trees
+for(x in numbers.to.add){
+  
+  create_new_trees(tree, number.to.add = x, 
+                   min.branch.length = 5, 
+                   youngest.date = 83.6, 
+                   oldest.date = 100.5, 
+                   path = "data/trees/", 
+                   simulation.name = paste0("tree_", x),
+                   split.by.clade = TRUE, 
+                   data = taxonomy, 
+                   species.col.name = "taxon", 
+                   clade.col.name = "clade")
+}
+
+# Get node counts for all full trees
+for(x in numbers.to.add){
+  # Read in tree
+  tree <- read.tree(paste0("data/trees/tree_", x, ".tre"))
+  # Get node counts
+  create_node_counts(tree, 
+                     path = "data/nodecounts/", 
+                     simulation.name = paste0("nodecount_", x))
+}
+
+# Get node counts for all clade trees
+for(x in numbers.to.add){
+  # Read in multiphylo object
+  tree.list <- read.tree(paste0("data/trees/tree_", x, "_clades.tre"))
+  
+  # Loop through each clade  
+  for(j in 1:length(tree.list)){
+    # Split multi phylo into one tree
+    tree <- tree.list[[j]]
+    # Get node counts
+    create_node_counts(tree, path = "data/nodecounts/", 
+                       simulation.name = paste0("nodecount_", x , "_", 
+                                                names(tree.list[j])))
+    }
+}
