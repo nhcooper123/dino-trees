80611a3c293a52233384fa42a2f14f8bd54194c3
Natalie Cooper <ncooper@tcd.ie>
2018-03-14 18:42:34 +0000

    Run prediction figures code

---
+++ analyses/05-figures-predictions.R
@@ -0,0 +1,47 @@
+# Load libraries
+library(tidyverse)
+library(MCMCglmm)
+library(gridExtra)
+source("functions/get_predictions.R")
+source("functions/make_prediction_figures.R")
+
+# Add rphylopic
+install.packages("remotes")
+remotes::install_github("sckott/rphylopic")
+library(rphylopic) 
+
+# Colour blind friendly palette
+# First is grey
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", 
+               "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+# Add silhouettes
+library(png)
+img_sauro <- readPNG("outputs/sauropod.png")
+img_orni <- readPNG("outputs/stegosaurus.png")
+img_thero <- readPNG("outputs/velociraptor.png")
+
+# Make figures
+# Original trees
+make_predictions_figures(treename  = "lloyd2008", rep = "", 
+                         col1 = cbPalette[4], col2 = cbPalette[3])
+
+# 1% extra
+make_predictions_figures(treename  = "4", rep = "_33", 
+                         col1 = cbPalette[4], col2 = cbPalette[3])
+
+# 5% extra
+make_predictions_figures(treename  = "21", rep = "_21", 
+                         col1 = cbPalette[4], col2 = cbPalette[3])
+
+# 10% extra
+make_predictions_figures(treename  = "42", rep = "_14", 
+                         col1 = cbPalette[4], col2 = cbPalette[3])
+
+# 25% extra
+make_predictions_figures(treename  = "105", rep = "_11", 
+                         col1 = cbPalette[4], col2 = cbPalette[3])
+
+# 50% extra
+make_predictions_figures(treename  = "210", rep = "_17", 
+                         col1 = cbPalette[4], col2 = cbPalette[3])
