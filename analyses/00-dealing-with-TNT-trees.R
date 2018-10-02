3b397b2ae1a9eb6b1e42efdb1fd6e5a13b77e3c7
Natalie Cooper <ncooper@tcd.ie>
2018-10-02 14:08:26 +0100

    Fix the issue with names and dates not matching

--- analyses/00-dealing-with-TNT-trees.R
+++ analyses/00-dealing-with-TNT-trees.R
@@ -4,57 +4,52 @@ library(paleotree)
 library(BioGeoBEARS)
 library(tidyverse)
 
-# Read in Matzke code for dealing with TNT outputs
+# Read in Nick Matzke's code for dealing with TNT outputs
 source("functions/tnt_R_utils_v1.R")
 
 # Read in PBDB dates 
-sauro_pbdb <- read_csv("data/trees_TNT/sauropod-dates-PBDB.csv")
-thero_pbdb <- read_csv("data/trees_TNT/theroopod-dates-PBDB.csv")
-orni_pbdb <- read_csv("data/trees_TNT/orni-dates-PBDB.csv")
+dino_pbdb <- read_csv("data/trees_TNT/dino-dates-PBDB.csv")
 
 # Split up names column so they can be matched
 # select just Genus and date columns
-sauro_dates <- sauro_pbdb %>%
+dino_dates <- dino_pbdb %>%
   mutate(split.names = accepted_name) %>%
   separate(split.names, sep = " ", c("Genus", "species")) %>%
-  select(Genus, max_ma, min_ma)
+  unite(binomial, Genus, species, sep = "_", remove = FALSE) %>%
+  select(binomial, Genus, max_ma, min_ma) %>%
+  gather(taxon, binomial:Genus, -min_ma, -max_ma) %>%
+  rename(name = `binomial:Genus`)
 
-thero_dates <- thero_pbdb %>%
-  mutate(split.names = accepted_name) %>%
-  separate(split.names, sep = " ", c("Genus", "species")) %>%
-  select(Genus, max_ma, min_ma)
+# Extract list of trees from folder
+tree.list <- list.files("data/trees_TNT")[str_which(list.files("data/trees_TNT"), 
+                                                    pattern = ".tre")]
 
-orni_dates <- orni_pbdb %>%
-  mutate(split.names = accepted_name) %>%
-  separate(split.names, sep = " ", c("Genus", "species")) %>%
-  select(Genus, max_ma, min_ma)
+for (i in 1:length(tree.list)){
 
+  # Read in the tree and select MPR tree
+  tree1 <- tntfile2R(paste0("data/trees_TNT/", tree.list[i]))
+  tree1 <- tree1[[1]]
+  tree_names <- data.frame(names = tree1$tip.label)
 
+  # Create dates dataset
+  dates <- left_join(tree_names, dino_dates, 
+                   by = c("names" = "name"))
 
+  # Select just first occassion
+  # This requires finessing in final analyses
+  dates2 <- dates[!duplicated(dates$names), ]
 
-# Read in the tree and select MPR tree
-tree1 <- tntfile2R("data/trees_TNT/GonzalezRiga.tre")
-tree1 <- tree1[[1]]
-tree_names <- data.frame(names = tree1$tip.label)
+  # Remove names column and add it to row names
+  timeData <- dates2[, 2:3]
+  rownames(timeData) <- dates2$names
 
+  # Date the tree...
+  tree_dated <- timePaleoPhy(tree1, timeData, type = "mbl", 
+                             vartime = 1, ntrees = 100,
+                             dateTreatment = "minMax", 
+                             noisyDrop = TRUE, plot = TRUE)
 
-  
-# Create dates dataset
-dates <- left_join(tree_names, sauro_dates, 
-                   by = c("names" = "Genus"))
+  write.nexus(tree_dated, file = paste0(tree.list[i], ".nex"))
+              
+}
 
-# Select just first occassion
-# This requires finessing in final analyses
-dates2 <- dates[!duplicated(dates$names), ]
-
-# Remove names column and add it to row names
-timeData <- dates2[, 2:3]
-rownames(timeData) <- dates2$names
-
-# Date the tree...
-tree_dated <- timePaleoPhy(tree1, timeData, type = "mbl", 
-                           vartime = 1, ntrees = 100,
-                           dateTreatment = "minMax", 
-                           noisyDrop = TRUE, plot = TRUE)
-
-write.nexus(tree_dated, file = "GonzalezRiga_dated.nex")
