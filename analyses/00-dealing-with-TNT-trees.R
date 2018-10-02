984b7c5ef530aea524dc1c2c61e7c73952c9fc9c
Natalie Cooper <ncooper@tcd.ie>
2018-10-02 13:32:54 +0100

    Add dates for all clades of dinosaurs

--- analyses/00-dealing-with-TNT-trees.R
+++ analyses/00-dealing-with-TNT-trees.R
@@ -7,21 +7,37 @@ library(tidyverse)
 # Read in Matzke code for dealing with TNT outputs
 source("functions/tnt_R_utils_v1.R")
 
+# Read in PBDB dates 
+sauro_pbdb <- read_csv("data/trees_TNT/sauropod-dates-PBDB.csv")
+thero_pbdb <- read_csv("data/trees_TNT/theroopod-dates-PBDB.csv")
+orni_pbdb <- read_csv("data/trees_TNT/orni-dates-PBDB.csv")
+
+# Split up names column so they can be matched
+# select just Genus and date columns
+sauro_dates <- sauro_pbdb %>%
+  mutate(split.names = accepted_name) %>%
+  separate(split.names, sep = " ", c("Genus", "species")) %>%
+  select(Genus, max_ma, min_ma)
+
+thero_dates <- thero_pbdb %>%
+  mutate(split.names = accepted_name) %>%
+  separate(split.names, sep = " ", c("Genus", "species")) %>%
+  select(Genus, max_ma, min_ma)
+
+orni_dates <- orni_pbdb %>%
+  mutate(split.names = accepted_name) %>%
+  separate(split.names, sep = " ", c("Genus", "species")) %>%
+  select(Genus, max_ma, min_ma)
+
+
+
+
 # Read in the tree and select MPR tree
 tree1 <- tntfile2R("data/trees_TNT/GonzalezRiga.tre")
 tree1 <- tree1[[1]]
 tree_names <- data.frame(names = tree1$tip.label)
 
-# Read in PBDB dates 
-sauro_pbdb <- read_csv("data/trees_TNT/sauropod-dates-PBDB.csv")
 
-# Split up names column so they can be matched
-# select just Genus and date columns
-# merge with tree names
-sauro_dates <- sauro_pbdb %>%
-  mutate(split.names = accepted_name) %>%
-  separate(split.names, sep = " ", c("Genus", "species")) %>%
-  select(Genus, max_ma, min_ma)
   
 # Create dates dataset
 dates <- left_join(tree_names, sauro_dates, 
