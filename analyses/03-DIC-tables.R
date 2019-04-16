84d9e7a945f9811937cfd500cbaa068d6d5ea4c8
Natalie Cooper <ncooper@tcd.ie>
2019-04-16 18:23:42 +0100

    Edits to DIC tables code

--- analyses/03-DIC-tables.R
+++ analyses/03-DIC-tables.R
@@ -9,43 +9,33 @@ library(xtable)
 ds <- read.csv("outputs/mcmcglmm_outputs.csv")
 dsi <- read.csv("outputs/mcmcglmm_outputs_intercepts.csv")
 
-# Group by clade and tree then get mean DIC values
-# across 50 replicates
-dic <- 
-  ds %>%
-  group_by(treeID, clade) %>%
-  select(1:2, 4:6) %>% 
-  summarise(meannull = median(null_DIC),
-            meanslow = median(slow_DIC),
-            meanasym = median(asym_DIC))
-
-dici <- 
-  dsi %>%
-  group_by(treeID, clade) %>%
-  select(1:2, 4:6) %>% 
-  summarise(meannull = median(null_DIC),
-            meanslow = median(slow_DIC),
-            meanasym = median(asym_DIC))
-
-# Create tidy names for number of taxa added
+# Create tidy names for trees
 # and clade names
-  taxa <- c(rep("-", 4),
-            rep("1%", 4), 
-            rep("5%", 4),
-            rep("10%", 4),
-            rep("25%", 4), 
-            rep("50%", 4))
+  tree <- c("Arbour", 
+            rep("Benson et al", 4), "Carbadillo et al", "Cau et al",
+            "Chiba et al", "Cruzado et al", "Gonzalez Riga et al",
+            rep("Lloyd et al", 4), 
+            "Mallon et al", "Raven & Maidment", "Thompson et al")
   
-  clade <- rep(c("all", "ornithischians", 
-                 "sauropods", "theropods"), 6)
+  clade <- c("Ornithischia",
+             "Ornithischia", "Sauropoda", "Theropoda", "All", 
+             "Sauropoda",
+             "Theropoda",
+             "Ornithischia",
+             "Ornithischia",
+             "Sauropoda",
+             "Ornithischia", "Sauropoda", "Theropoda", "All",
+             "Ornithischia",
+             "Ornithischia",
+             "Ornithischia")
             
 # Formatting the tables
-table_out <- data.frame(taxa, clade, dic[, 3:5])
-colnames(table_out) <- c("% taxa added", "clade", "null DIC",
+table_out <- data.frame(tree, clade, dic[, 3:5])
+colnames(table_out) <- c("tree", "clade", "null DIC",
                          "slowdown DIC", "asymptote DIC")
 
-table_outi <- data.frame(taxa, clade, dici[, 3:5])
-colnames(table_outi) <- c("% taxa added", "clade", "null DIC",
+table_outi <- data.frame(tree, clade, dici[, 3:5])
+colnames(table_outi) <- c("tree", "clade", "null DIC",
                          "slowdown DIC", "asymptote DIC")
   
 # Save tables as .tex files
