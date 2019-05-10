417663d069ac859009511fd2584c3d27a38ad223
Natalie Cooper <ncooper@tcd.ie>
2019-05-10 16:21:16 +0100

    Simplify code now there  are only multitree objects

--- analyses/02-run-mcmcglmm-models.R
+++ analyses/02-run-mcmcglmm-models.R
@@ -39,19 +39,15 @@ output2 <- make_mcmc_output_intercept(ntrees = 800)
 #----------------------------------------------------------------
 # Run models
 #----------------------------------------------------------------
-tree.no <- 0 # start counter
+tree.no <- 1 # start counter
 
 for(i in 1:length(tree.list)){
-  
-  tree.no <- tree.no + 1
   
   # Read in tree
   tree <- read.nexus(paste0("data/trees/", tree.list[[i]]))
   
-  # If there is > 1 tree then run on all 100 dated trees
-  if(class(tree) == "multiPhylo"){
-    
-    for(j in 1:100){
+  # Run on all 100 dated trees
+  for(j in 1:100){
       
       tree1 <- tree[[j]]
       tree1$node.label <- NULL
@@ -85,33 +81,4 @@ for(i in 1:length(tree.list)){
       # Add to counter
       tree.no <- tree.no + 1
     }
-  }
-  else{
-    
-    # Load the node count data
-    # Note this breaks if you use read_csv
-    nodecount.data <- read.csv(paste0("data/nodecounts/nodecount_", 
-                                      tree.list[i], ".csv"))
-    
-    # Run the models
-    model.outputs <- run_three_models(tree, nodecount.data, prior, nitt, thin, burnin)
-    model.outputs2 <- run_three_models_intercept(tree, nodecount.data, prior, nitt, thin, burnin)
-    
-    # Add model outputs
-    output <- add_mcmc_output(output, null.model = model.outputs[[1]], 
-                              slow.model = model.outputs[[2]], 
-                              asym.model = model.outputs[[3]], 
-                              tree.no = tree.no,
-                              tree.name = tree.list[i])
-    
-    output2 <- add_mcmc_output_intercept(output2, null.model = model.outputs2[[1]], 
-                                         slow.model = model.outputs2[[2]], 
-                                         asym.model = model.outputs2[[3]], 
-                                         tree.no = tree.no,
-                                         tree.name = tree.list[i])
-    
-    # Save the outputs
-    write_csv(output, path = "outputs/mcmcglmm_outputs.csv")
-    write_csv(output2, path = "outputs/mcmcglmm_outputs_intercepts.csv")
-  }
 }
