ed4b70617f205b6b4274b8d240f9c1ddc73b00c6
Natalie Cooper <natalie.cooper@nhm.ac.uk>
2019-05-11 15:45:00 +0100

    Add brackets and phylopics

--- analyses/05-figures-best-models.R
+++ analyses/05-figures-best-models.R
@@ -6,6 +6,14 @@
 #----------------
 library(tidyverse)
 library(rphylopic)
+library(png)
+library(pBrackets)
+#-------------------
+# Add pics
+#-------------------
+steg <- readPNG("img/stegosaurus.png")
+raptor <- readPNG("img/velociraptor.png")
+pod <- readPNG("img/sauropod.png")
 
 #648FFF = blue
 #785EF0 = purple
@@ -45,7 +53,22 @@ ggplot(ds, aes(x = tree, y = best, fill = best_model)) +
         strip.background = element_rect(fill = "white"),
         legend.spacing.x = unit(0.2, 'cm'),
         axis.text.x = element_text(angle = 45, hjust = 1)) +
+  add_phylopic(img = steg, alpha = 1, x = 3.5, y = 85, ysize = 15) +
+  add_phylopic(img = pod, alpha = 1, x = 7.5, y = 86, ysize = 18) +
+  add_phylopic(img = raptor, alpha = 1, x = 8.9, y = 83, ysize = 12)
+
+grid.brackets(52, 68, 222, 68, lwd = 2, col = "black",
+              type = 4, tick = NA)
+grid.brackets(226, 68, 280, 68, lwd = 2, col = "black",
+              type = 4, tick = NA)
+grid.brackets(284, 68, 310, 68, lwd = 2, col = "black",
+              type = 4, tick = NA)
+
+# Save plot via export
   
+library(grid)
+grid.locator(unit="native") 
+
 
 # Intercept
 ggplot(ds_i, aes(x = tree, y = best, fill = best_model)) +
@@ -53,12 +76,20 @@ ggplot(ds_i, aes(x = tree, y = best, fill = best_model)) +
   theme_bw(base_size = 14) +
   scale_fill_manual(values = c("grey", "#785EF0", "#DC267F"),
                     labels = c("none", "asymptote/downturn", "downturn")) +
-  labs(x = "", y = "% trees", fill = "best model") +
+  labs(x = "", y = "% trees", fill = "") +
   theme(legend.position = "top",
         strip.background = element_rect(fill = "white"),
         legend.spacing.x = unit(0.2, 'cm'),
-        axis.text.x = element_text(angle = 45, hjust = 1))
+        axis.text.x = element_text(angle = 45, hjust = 1)) +
+  add_phylopic(img = steg, alpha = 1, x = 12.5, y = 85, ysize = 15) +
+  add_phylopic(img = pod, alpha = 1, x = 16.5, y = 86, ysize = 18) +
+  add_phylopic(img = raptor, alpha = 1, x = 17.9, y = 83, ysize = 10)
 
-
-
-add_phylopic(img = img_frog, alpha = 1, x = 4, y = 90, ysize = 10) +
+grid.brackets(50, 68, 179, 68, lwd = 2, col = "black",
+              type = 4, tick = NA)
+grid.brackets(181, 68, 268, 68, lwd = 2, col = "black",
+              type = 4, tick = NA)
+grid.brackets(270, 68, 297, 68, lwd = 2, col = "black",
+              type = 4, tick = NA)
+grid.brackets(299, 68, 313, 68, lwd = 2, col = "black",
+              type = 4, tick = NA)
