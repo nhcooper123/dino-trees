aff9d3bdcbb9c10719c82b9e9985844531117f29
Natalie Cooper <natalie.cooper@nhm.ac.uk>
2019-05-11 14:57:16 +0100

    Fix factor relevelling

--- analyses/05-figures-best-models.R
+++ analyses/05-figures-best-models.R
@@ -5,6 +5,7 @@
 # Load libraries
 #----------------
 library(tidyverse)
+library(rphylopic)
 
 #648FFF = blue
 #785EF0 = purple
@@ -17,21 +18,34 @@ library(tidyverse)
 ds <- read_csv("outputs/best-models.csv")
 ds_i <- read_csv("outputs/best-models-intercepts.csv")
 
-# Relevel to order by model as required
+# Relevel to order by model as required and by clade
+ds <- ds %>%
+  mutate(tree = fct_relevel(tree, 
+                            "Arbour", "Chiba", "Cruzado", "Mallon", "Raven", "Thompson",
+                            "Carbadillo", "GonzalezR",
+                            "Cau"))
+
 ds_i <- ds_i %>%
-  mutate(best_model = relevel(as.factor(best_model), ref = "none"))
+  mutate(best_model = fct_relevel(best_model, "none")) %>%
+  mutate(tree = fct_relevel(tree, "Benson1", "Benson2", "Lloyd",
+                            "Benson1_3", "Benson2_3", "Lloyd_3",
+                            "Benson1_5", "Benson2_5", "Lloyd_5",
+                            "Arbour", "Chiba", "Cruzado", "Mallon", "Raven", "Thompson",
+                            "Carbadillo", "GonzalezR",
+                            "Cau"))
 
 # No intercept
 ggplot(ds, aes(x = tree, y = best, fill = best_model)) +
-  geom_bar(stat = "identity", alpha = 1, position = position_dodge()) +
+  geom_bar(stat = "identity", alpha = 1) +
   theme_bw(base_size = 14) +
   scale_fill_manual(values = c("#648FFF", "#785EF0"),
                     labels = c("asymptote", "asymptote/downturn")) +
-  labs(x = "", y = "% trees", fill = "best model") +
+  labs(x = "", y = "% trees", fill = "") +
   theme(legend.position = "top",
         strip.background = element_rect(fill = "white"),
         legend.spacing.x = unit(0.2, 'cm'),
-        axis.text.x = element_text(angle = 45, hjust = 1))
+        axis.text.x = element_text(angle = 45, hjust = 1)) +
+  
 
 # Intercept
 ggplot(ds_i, aes(x = tree, y = best, fill = best_model)) +
@@ -46,5 +60,5 @@ ggplot(ds_i, aes(x = tree, y = best, fill = best_model)) +
         axis.text.x = element_text(angle = 45, hjust = 1))
 
 
-library(rphylopic)
+
 add_phylopic(img = img_frog, alpha = 1, x = 4, y = 90, ysize = 10) +
