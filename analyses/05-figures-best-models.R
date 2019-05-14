a74feff1c6e93607c9fef87182323f86cfad0d4c
Natalie Cooper <ncooper@tcd.ie>
2019-05-14 11:25:39 +0100

    Modify for new Benson and Lloyd results

--- analyses/05-figures-best-models.R
+++ analyses/05-figures-best-models.R
@@ -29,15 +29,15 @@ ds_i <- read_csv("outputs/best-models-intercepts.csv")
 # Relevel to order by model as required and by clade
 ds <- ds %>%
   mutate(tree = fct_relevel(tree, 
+                            "Benson1", "Benson2", "Lloyd",
                             "Arbour", "Chiba", "CruzadoC", "Mallon", "Raven", "Thompson",
                             "Carballido", "GonzalezR",
                             "Cau"))
 
 ds_i <- ds_i %>%
   mutate(best_model = fct_relevel(best_model, "none")) %>%
-  mutate(tree = fct_relevel(tree, "Benson1", "Benson2", "Lloyd",
-                            "Benson1_3", "Benson2_3", "Lloyd_3",
-                            "Benson1_5", "Benson2_5", "Lloyd_5",
+  mutate(tree = fct_relevel(tree, 
+                            "Benson1", "Benson2", "Lloyd",
                             "Arbour", "Chiba", "CruzadoC", "Mallon", "Raven", "Thompson",
                             "Carballido", "GonzalezR",
                             "Cau"))
@@ -46,8 +46,8 @@ ds_i <- ds_i %>%
 ggplot(ds, aes(x = tree, y = best, fill = best_model)) +
   geom_bar(stat = "identity", alpha = 1) +
   theme_bw(base_size = 14) +
-  scale_fill_manual(values = c("#648FFF", "#785EF0"),
-                    labels = c("asymptote", "asymptote/downturn")) +
+  scale_fill_manual(values = c("#648FFF", "#785EF0", "#DC267F"),
+                    labels = c("asymptote", "asymptote/downturn", "downturn")) +
   labs(x = "", y = "% trees", fill = "") +
   theme(legend.position = "top",
         strip.background = element_rect(fill = "white"),
