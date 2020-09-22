9076fba6efde19f8bd4ba0778d2533b3538cf573
Natalie Cooper <natalie.cooper@nhm.ac.uk>
2020-09-22 17:38:11 +0100

    Extract root ages for revision

--- analyses/07-extract-root-ages.R
+++ analyses/07-extract-root-ages.R
@@ -6,6 +6,9 @@ library(ape)
 library(tidyverse)
 library(paleotree)
 
+#----------------------------------------------
+# Extract root ages for paleotree
+#----------------------------------------------
 # Extract list of trees and dates from folder
 tree.list <- list.files("data/trees_TNT", pattern = ".phy")
 dates_files <- list.files("data/trees_dates", pattern = ".csv")
@@ -42,6 +45,8 @@ for (i in 1:length(tree.list)){
     # Add tree name and age to output
     output$tree[tree.no] <- tree.list[i]
     output$age[tree.no] <- tree_dated[[j]]$root.time
+    output$max[tree.no] <- max(dates1$max_ma, na.rm = TRUE)
+    output$min[tree.no] <- min(dates1$min_ma, na.rm = TRUE)
   
     # Add to counter
     tree.no <- tree.no+1
@@ -50,3 +55,61 @@ for (i in 1:length(tree.list)){
 
 # Save output
 write_csv(output, path = "outputs/root-ages.csv")
+
+#----------------------------------------------
+# Plot results
+#----------------------------------------------
+ages <- read_csv("outputs/root-ages.csv")
+
+# Change tree names for plotting and order based on group
+ages <-
+  ages %>%
+  mutate(tree = case_when(tree == "Arbour.phy" ~ "Arbour",
+                          tree == "Carballido.phy" ~ "Carballido",
+                          tree == "Cau.phy" ~ "Cau",
+                          tree == "Chiba.phy" ~ "Chiba",
+                          tree == "Cruzado.phy" ~ "CruzadoC",
+                          tree == "GonzalezRiga.phy" ~ "GonzalezR",
+                          tree == "Mallon.phy" ~ "Mallon",
+                          tree == "Raven.phy" ~ "Raven",
+                          tree == "Thompson.phy" ~ "Thompson")) %>%
+  mutate(tree = fct_relevel(tree, 
+                            "Arbour", "Chiba", "CruzadoC", "Mallon", "Raven", "Thompson",
+                            "Carballido", "GonzalezR",
+                            "Cau"))
+
+# Create dataset for max and min ages which differ depending on the tree
+dates <- 
+  ages %>%
+  group_by(tree) %>%
+  summarise(max_age = max(max),
+            min_age = min(min)) %>%
+  mutate(overall_max = c(rep(201,8), 231))
+  
+# Plot
+ggplot(ages, aes(x = age)) +
+  geom_density(alpha = 0.5) +
+  facet_wrap(~tree, nrow = 3) +
+  theme_bw(base_size = 14) +
+  theme(strip.background = element_rect(fill = "white"),
+        axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_x_continuous(breaks= pretty_breaks()) +
+  scale_y_continuous(breaks= pretty_breaks()) +
+  geom_segment(data = dates,
+               aes(x = max_age, y = 0, 
+                   xend = max_age, 
+                   yend = 0.4), inherit.aes = FALSE,
+             linetype = "dashed", colour = "#d1495b") +
+  geom_segment(data = dates,
+               aes(x = min_age, y = 0, 
+                   xend = min_age, 
+                   yend = 0.4), inherit.aes = FALSE,
+               linetype = "dashed", colour = "#00798c") +
+  geom_segment(data = dates,
+               aes(x = overall_max, y = 0, 
+                   xend = overall_max, 
+                   yend = 0.4), inherit.aes = FALSE,
+               linetype = "dotted", colour = "black") +
+  xlab("root age (Ma)")
+
+#ggsave("outputs/figure-root-ages.png", width = 20, height = 20, units = "cm")
