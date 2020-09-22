86a7e479d4f12aea6c294edcdc173cdacf81c566
Natalie Cooper <natalie.cooper@nhm.ac.uk>
2020-09-22 16:25:11 +0100

    Extract root ages for paleotree outputs

--- analyses/07-extract-root-ages.R
+++ analyses/07-extract-root-ages.R
@@ -4,33 +4,49 @@
 # Load libraries
 library(ape)
 library(tidyverse)
+library(paleotree)
 
-# List trees
-tree.list <- list.files("data/trees", pattern = ".nex")
+# Extract list of trees and dates from folder
+tree.list <- list.files("data/trees_TNT", pattern = ".phy")
+dates_files <- list.files("data/trees_dates", pattern = ".csv")
 
 # Make blank output
 output <- data.frame(tree = rep(NA, 900), age = rep(NA, 900))
 
 # Start counter
 tree.no <- 1
-# Loop through trees and output root ages
-for(i in 1:length(tree.list)){
+
+for (i in 1:length(tree.list)){
   
-  # Read in trees
-  tree <- read.nexus(paste0("data/trees/", tree.list[[i]]))
+  # Read in the tree and dates
+  tree1 <- read.tree(paste0("data/trees_TNT/", tree.list[i]))
+  dates1 <- read_csv(paste0("data/trees_dates/", dates_files[i]))
   
-  # Run on all 100 dated trees
+  # If there is > 1 tree then select first tree - 
+  # this can be an error with TNT which outputs two consensus trees for no reason
+  if(class(tree1) == "multiPhylo"){
+    tree1 <- tree1[[1]]
+  }
+  
+  # Remove names column and add it to row names
+  timeData <- dates1[, 2:3]
+  rownames(timeData) <- dates1$name
+  
+  # Date the tree...
+  tree_dated <- timePaleoPhy(tree1, timeData, type = "mbl", 
+                             vartime = 1, ntrees = 100,
+                             dateTreatment = "minMax", 
+                             noisyDrop = TRUE, plot = FALSE)
+  
   for(j in 1:100){
-    tree1 <- tree[[j]]
-    
     # Add tree name and age to output
     output$tree[tree.no] <- tree.list[i]
-    output$age[tree.no] <- max(branching.times(tree1))
-   
-     # Add to counter
+    output$age[tree.no] <- tree_dated[[j]]$root.time
+  
+    # Add to counter
     tree.no <- tree.no+1
   }
-}    
+}  
 
 # Save output
 write_csv(output, path = "outputs/root-ages.csv")
