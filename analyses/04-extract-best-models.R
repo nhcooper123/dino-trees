1e642d0376818b624c1b18e33a13dd679248e083
Natalie Cooper <natalie.cooper@nhm.ac.uk>
2020-09-23 14:46:18 +0100

    Add code for offset models

--- analyses/04-extract-best-models.R
+++ analyses/04-extract-best-models.R
@@ -67,13 +67,42 @@ to_plot_i <-
   select(tree, best_model, best) %>%
   distinct()
 
+#-------------------------------
+# Intercept = 1.0
+#-------------------------------
+# Read in data
+ds_offset <- read_csv("outputs/mcmcglmm_outputs_offset.csv")
+
+# Extract best models
+# Get delta DICs then pick best models based on 4 units DIC difference
+sum_all_o <-
+  ds_offset %>%
+  mutate(null_asym = null_DIC - asym_DIC) %>%
+  mutate(null_slow = null_DIC - slow_DIC) %>%
+  mutate(asym_slow = asym_DIC - slow_DIC) %>%
+  mutate(best_model = case_when(null_asym < -4 & null_slow < -4 ~ "null",
+                                null_asym > 4 & asym_slow < -4 ~ "asymptote",
+                                null_slow > 4 & asym_slow > 4 ~ "slowdown",
+                                null_asym > 4 & null_slow > 4 & (asym_slow > -4 &  asym_slow < 4) ~ "asymptote/slowdown",
+                                (null_asym < 4 & null_asym > -4) & null_slow > 4 & asym_slow < -4 ~ "null/asympote",
+                                null_asym > 4 & (null_slow < 4 & null_slow > -4) & asym_slow > 4 ~ "null/slowdown",
+                                abs(null_asym) < 4 & abs(null_slow) < 4 & abs(asym_slow) < 4 ~ "none")) %>%
+  mutate(best_model = if_else(is.na(best_model) == TRUE, "none", best_model))
+
+# Get % of trees supporting each model
+to_plot_o <- 
+  sum_all_o %>%
+  group_by(tree) %>%
+  add_count(best_model, name = "best") %>%
+  select(tree, best_model, best) %>%
+  distinct()
+
 #-------------------------------------
 # Sakamoto results
 # No intercepts
 #-------------------------------------
 # Read in data
 ds_saka <- read_csv("outputs/mcmcglmm_outputs_sakamoto.csv")
-ds_saka_intercepts <- read_csv("outputs/mcmcglmm_outputs_intercepts_sakamoto.csv")
 
 # Separate dates from tree names
 ds_saka <- ds_saka %>%
@@ -143,6 +172,44 @@ to_plot_saka_i <-
   # Make into % for comparison with other data
   mutate(best = best * (100/3))
 
+#-------------------------------------
+# Sakamoto results
+# Intercept = 1.0
+#-------------------------------------
+# Read in data
+ds_saka_o <- read_csv("outputs/mcmcglmm_outputs_offset_sakamoto.csv")
+
+# Separate dates from tree names
+ds_saka_o <- ds_saka_o %>%
+  separate(tree, c("tree", "dating"), sep = "_")
+
+
+# Extract best models
+# Get delta DICs then pick best models based on 4 units DIC difference
+sum_all_saka_o <-
+  ds_saka_o %>%
+  mutate(null_asym = null_DIC - asym_DIC) %>%
+  mutate(null_slow = null_DIC - slow_DIC) %>%
+  mutate(asym_slow = asym_DIC - slow_DIC) %>%
+  mutate(best_model = case_when(null_asym < -4 & null_slow < -4 ~ "null",
+                                null_asym > 4 & asym_slow < -4 ~ "asymptote",
+                                null_slow > 4 & asym_slow > 4 ~ "slowdown",
+                                null_asym > 4 & null_slow > 4 & (asym_slow > -4 &  asym_slow < 4) ~ "asymptote/slowdown",
+                                (null_asym < 4 & null_asym > -4) & null_slow > 4 & asym_slow < -4 ~ "null/asympote",
+                                null_asym > 4 & (null_slow < 4 & null_slow > -4) & asym_slow > 4 ~ "null/slowdown",
+                                abs(null_asym) < 4 & abs(null_slow) < 4 & abs(asym_slow) < 4 ~ "none")) %>%
+  mutate(best_model = if_else(is.na(best_model) == TRUE, "none", best_model))
+
+# Get % of trees supporting each model
+to_plot_saka_o <- 
+  sum_all_saka_o %>%
+  group_by(tree) %>%
+  add_count(best_model, name = "best") %>%
+  select(tree, best_model, best) %>%
+  distinct() %>%
+  # Make into % for comparison with other data
+  mutate(best = best * (100/3))
+
 #------------------------------------- 
 # Combine with datasets together
 # No intercepts
@@ -192,3 +259,28 @@ plot_data_i <-
                           tree == "lloyd" ~ "Lloyd"))
 # Write to file
 write_csv(plot_data_i, path = "outputs/best-models-intercepts.csv")
+
+#------------------------------------- 
+# Combine with datasets together
+# With intercepts
+#-------------------------------------
+plot_data_o <- rbind(to_plot_o, to_plot_saka_o)
+
+# Tidy up tree names
+plot_data_o <-
+  plot_data_o %>%
+  ungroup(tree) %>%
+  mutate(tree = case_when(tree == "Arbour.phy.nex" ~ "Arbour",
+                          tree == "Carballido.phy.nex" ~ "Carballido",
+                          tree == "Cau.phy.nex" ~ "Cau",
+                          tree == "Chiba.phy.nex" ~ "Chiba",
+                          tree == "Cruzado.phy.nex" ~ "CruzadoC",
+                          tree == "GonzalezRiga.phy.nex" ~ "GonzalezR",
+                          tree == "Mallon.phy.nex" ~ "Mallon",
+                          tree == "Raven.phy.nex" ~ "Raven",
+                          tree == "Thompson.phy.nex" ~ "Thompson",
+                          tree == "benson1" ~ "Benson1",
+                          tree == "benson2" ~ "Benson2",
+                          tree == "lloyd" ~ "Lloyd"))
+# Write to file
+write_csv(plot_data_o, path = "outputs/best-models-offset.csv")
